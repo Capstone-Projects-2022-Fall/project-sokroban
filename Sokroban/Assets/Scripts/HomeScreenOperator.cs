ec4bae7864aab5bbbb94c66279eb4c09add5227e
robnano <tun17440@temple.edu>
2022-10-01 13:39:54 -0400

    Home Screen
    SOK-27

--- Sokroban/Assets/Scripts/HomeScreenOperator.cs
+++ Sokroban/Assets/Scripts/HomeScreenOperator.cs
@@ -2,20 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 
 public class HomeScreenOperator : MonoBehaviour
 {
+    public InputField usernameInput;
+    public InputField passwordInput;
+    public Text errorText;
+
+
+
     public void OnClickLogin()
     {
-        SceneManager.LoadScene("Main Menu");
-        Debug.Log("Entered Main Menu");
+        if (string.IsNullOrEmpty(usernameInput.text) || string.IsNullOrEmpty(passwordInput.text))
+        {
+            errorText.text = "Input Username and Password!";
+            Debug.Log("Bad credentials");
+            return;
+        } 
+        else {
+            
+            SceneManager.LoadScene("Main Menu");
+            Debug.Log("Entered Main Menu");
+        }
     }
 
     public void OnClickSignUp()
     {
-        SceneManager.LoadScene("Main Menu"); 
-        Debug.Log("Entered Main Menu");
+        if (string.IsNullOrEmpty(usernameInput.text) || string.IsNullOrEmpty(passwordInput.text))
+        {
+            errorText.text = "Input Username and Password!";
+            Debug.Log("Bad credentials");
+            return;
+        } 
+        else {
+            SceneManager.LoadScene("Main Menu"); 
+            Debug.Log("Entered Main Menu");
+        }
     }
 }
