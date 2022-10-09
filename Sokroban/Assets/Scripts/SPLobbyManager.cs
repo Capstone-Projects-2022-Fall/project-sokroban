0c78301ee49105b826fdc4e57063d5da88f0066b
Arthur Kozhevnik <kozhevnik1025@gmail.com>
2022-10-09 19:26:25 -0400

    Scaling Camera/ Proper Crate Input

--- Sokroban/Assets/Scripts/SPLobbyManager.cs
+++ Sokroban/Assets/Scripts/SPLobbyManager.cs
@@ -12,22 +12,17 @@ public class SPLobbyManager : MonoBehaviour
 
     public void OnSubmitClick()
     {
-        if (string.IsNullOrEmpty(numBoxes.text) || Convert.ToInt32(numBoxes.text) < 1)
+        if (string.IsNullOrEmpty(numBoxes.text) || Convert.ToInt32(numBoxes.text) < 1 || Convert.ToInt32(numBoxes.text) > 10)
         {
-            errorText.text = "Input valid number of boxes!";
+            errorText.text = "Input valid number of boxes! (Min:1 Max:10)";
             Debug.Log("Bad input");
             return;
         } 
         else {
             //Get the input, modify it to an int and pass it as argument in the level generator method. 
             SceneManager.LoadScene("SPLevel");
+            LevelTranslator.crates = Convert.ToInt32(numBoxes.text);
             Debug.Log("Entered Game Scene");
         }
-    }
-
-    public int getCrates()
-    {
-        Debug.Log(numBoxes.text+" crates spawned");
-        return Convert.ToInt32(numBoxes.text);
     }
 }
