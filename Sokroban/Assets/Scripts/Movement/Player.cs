7b05fbcaeb86d2efa48d861040c963e6ffdbfafd
Arthur Kozhevnik <kozhevnik1025@gmail.com>
2022-10-28 20:17:30 -0400

    Fixed else error

--- Sokroban/Assets/Scripts/Movement/Player.cs
+++ Sokroban/Assets/Scripts/Movement/Player.cs
@@ -219,7 +219,6 @@ public class Player : MonoBehaviour
 
     public void Undo()
     {
-        
         if(moves.Count > 0) //The stack has moves inside. In other words the player has done some moves
         {
             
@@ -236,12 +235,6 @@ public class Player : MonoBehaviour
                
             }
             moves.Pop();
-            
-        }
-        else 
-        {
-            Debug.Log("No moves to undo!");
-            return;
         }
         else
         {
