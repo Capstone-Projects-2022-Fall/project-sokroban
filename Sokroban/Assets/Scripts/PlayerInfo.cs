a3b27f162e6875851107e786657e5c4a141cd155
Felix Rabinovich <105725666+F-Rabinovich@users.noreply.github.com>
2022-11-09 20:24:53 -0500

    API connection
    Connected Sok to API, users can now create and account and sign up

---
+++ Sokroban/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo
+{
+    public string username;
+    public string password;
+}
--- Sokroban/Assets/Scripts/Screen Managers/HomeScreenManager.cs
+++ Sokroban/Assets/Scripts/Screen Managers/HomeScreenManager.cs
@@ -3,26 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Net.Http;
+
 
 
 public class HomeScreenManager : MonoBehaviour
 {
+    public string URLg;
+    public string URLp;
     public InputField usernameInput;
     public InputField passwordInput;
     public Text errorText;
+    public string password;
 
 
+
+    
+    public IEnumerator FetchData(string usr)
+    {
+
+        string uri = "https://sokroban.azurewebsites.net/Sokroban/Check?usr=" + usr;
+        using (UnityWebRequest request = UnityWebRequest.Get(uri))
+        {
+            yield return request.SendWebRequest();
+
+        
+        }
+    }
 
     public void OnClickLogin()
     {
         if (string.IsNullOrEmpty(usernameInput.text) || string.IsNullOrEmpty(passwordInput.text))
         {
             errorText.text = "Input Username and Password!";
+
             Debug.Log("Bad credentials");
             return;
-        } 
-        else {
-            
+        }
+        else
+        {
+            //FetchData(usernameInput.text);
             SceneManager.LoadScene("Main Menu");
             Debug.Log("Entered Main Menu");
         }
@@ -36,9 +57,26 @@ public class HomeScreenManager : MonoBehaviour
             Debug.Log("Bad credentials");
             return;
         } 
-        else {
+        else 
+        {
+            StartCoroutine(PostData(usernameInput.text,passwordInput.text));
             SceneManager.LoadScene("Main Menu"); 
             Debug.Log("Entered Main Menu");
         }
     }
+
+    IEnumerator PostData(string usr, string pass)
+    {
+     
+        string uri = "https://sokroban.azurewebsites.net/Sokroban/SokLogin?Username=" + usr + "&Password=" + pass;
+        WWWForm form = new WWWForm();
+        form.AddField("Username", "");
+        form.AddField("Password", "");
+        using (UnityWebRequest request = UnityWebRequest.Post(uri, form))
+        {
+            yield return request.SendWebRequest();
+        }
+    }
+
+
 }
