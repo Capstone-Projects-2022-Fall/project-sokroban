96c1321a7cd7643f24c9f2eb83d6c6840974b45f
Felix Rabinovich <105725666+F-Rabinovich@users.noreply.github.com>
2022-11-15 07:32:55 -0500

    Add files via upload

---
+++ Sokroban/Assets/Scripts/LevelStats.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LevelStats
+{
+    public int Level;
+    public string Username;
+    public int Score;
+    public int Time;
+}
+
