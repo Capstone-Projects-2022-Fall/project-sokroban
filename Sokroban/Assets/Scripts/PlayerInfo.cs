d42c896e6fc75968f72e90e952131ba828799558
Felix Rabinovich <105725666+F-Rabinovich@users.noreply.github.com>
2022-11-30 11:16:51 -0500

    Update PlayerInfo.cs
    Merging issues changing manually

--- Sokroban/Assets/Scripts/PlayerInfo.cs
+++ Sokroban/Assets/Scripts/PlayerInfo.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Net.Http;
+
 
 public class PlayerInfo
 {
-    public string username;
-    public string password;
+    public string Username;
+    public string Password;
 }
+
